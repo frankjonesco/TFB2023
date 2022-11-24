84eb261b1a51e705c5cf5c09131d578a98d3d573
“Frank <frank@frankjones.co>
2022-11-24 13:30:49 +0100

    CSS for table now in global vars

--- resources/views/dashboard/sectors/index.blade.php
+++ resources/views/dashboard/sectors/index.blade.php
@@ -3,26 +3,27 @@
     <h1>Sectors</h1>
     <p class="mb-9">Use sectors to organise your companies and news articles. Each sector also contains industry folders.</p>
     <x-alerts/>
-    <table class="table-auto w-full">
+
+    <table>
         <thead>
             <tr>
-                <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-left">Sector name</th>
-                <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-left">Last updated</th>
-                <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-left">Actions</th>
+                <th>Sector name</th>
+                <th>Last updated</th>
+                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
             @foreach($sectors as $sector)
-                <tr class="border-b border-slate-500 text-slate-500">
-                    <td class="p-4 pl-8">
-                        <a href="/dashboard/sectors/{{$sector->hex}}" class="hover:text-yellow-700">
+                <tr>
+                    <td>
+                        <a href="/dashboard/sectors/{{$sector->hex}}">
                             {{$sector->name}}
                         </a>
                     </td>
-                    <td class="p-4 pl-8">
+                    <td>
                         {{$sector->updated_at}}
                     </td>
-                    <td class="p-4 pl-8">
+                    <td>
                         <a href="/dashboard/sectors/{{$sector->hex}}/text/edit">
                             <button>
                                 <i class="fa-solid fa-marker"></i>
