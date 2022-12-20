3e8bb04c2823a218bb24bc96ee36716b71437663
“Frank <frank@frankjones.co>
2022-12-20 16:57:59 +0100

    Formatted login and sign up to 2/3 col 1/3 col.

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,36 +1,53 @@
 <x-layout>
     <x-container>
-        <h2>Log in</h2>
-        
-        <form action="/users/authenticate" method="POST" class="w-1/4">
-            @csrf
-
-            <div class="form-block">
-                <label for="last_name">Email</label>
-                <input type="email" name="email" placeholder="Email" value="{{old('email')}}">
-                @error('email')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
+        <div class="flex">
+            <div class="mt-6 pl-3 pr-12 w-2/3 border-r border-zinc-500">                
+                <div class="flex mb-7">
+                    <h3 class="pl-1.5 pr-4 pb-3 border-b border-sky-500 uppercase text-lg">
+                        Log in
+                    </h3>
+                    <span class="grow border-b border-zinc-500"></span>
+                </div>
+                <form action="/users/authenticate" method="POST" class="w-1/3">
+                    @csrf
+                    <div class="form-block">
+                        <label for="last_name">
+                            Email
+                        </label>
+                        <input type="email" name="email" placeholder="Email" value="{{old('email')}}">
+                        @error('email')
+                            <p class="text-red-500 text-xs mt-1">
+                                {{$message}}
+                            </p>
+                        @enderror
+                    </div>
+                    <div class="form-block">
+                        <label for="password">
+                            Password
+                        </label>
+                        <input type="password" name="password" placeholder="Password">
+                        @error('password')
+                            <p class="text-red-500 text-xs mt-1">
+                                {{$message}}
+                            </p>
+                        @enderror
+                    </div>
+                    <div class="form-block">
+                        <button type="submit" class="mr-1.5">
+                            Log in
+                        </button>
+                        <span class="text-sm">
+                            Don't have an account yet?
+                            <a href="/signup" class="underline underline-offset-2 ml-1.5">
+                                Sign up
+                            </a>
+                        </span>
+                    </div>
+                </form>
             </div>
-
-            <div class="form-block">
-                <label for="password">Password</label>
-                <input type="password" name="password" placeholder="Password">
-                @error('password')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
+            <div class="mt-6 px-12 w-1/3">
+                <x-module-socials />
             </div>
-
-            <div class="form-block">
-                <button type="submit">Log in</button>
-                <span class="text-sm">
-                    Don't have an account yet?
-                    <a href="/signup">
-                        Sign up
-                    </a>
-                </span>
-            </div>
-
-        </form>
+        </div>
     </x-container>
 </x-layout>
--- resources/views/users/signup.blade.php
+++ resources/views/users/signup.blade.php
@@ -1,69 +1,80 @@
 <x-layout>
     <x-container>
-        <h1>Sign up</h1>
-        
-        <form action="/users/store" method="POST" class="w-1/4">
-            @csrf
+        <div class="flex">
+            <div class="mt-6 pl-3 pr-12 w-2/3 border-r border-zinc-500">                
+                <div class="flex mb-7">
+                    <h3 class="pl-1.5 pr-4 pb-3 border-b border-sky-500 uppercase text-lg">
+                        Sign up
+                    </h3>
+                    <span class="grow border-b border-zinc-500"></span>
+                </div>
+                <form action="/users/store" method="POST" class="w-1/3">
+                    @csrf
 
-            <div class="form-block">
-                <label for="first_name">First name</label>
-                <input type="text" name="first_name" placeholder="First name" value="{{old('first_name')}}">
-                @error('first_name')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
+                    <div class="form-block">
+                        <label for="first_name">First name</label>
+                        <input type="text" name="first_name" placeholder="First name" value="{{old('first_name')}}">
+                        @error('first_name')
+                            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <div class="form-block">
+                        <label for="last_name">Last name</label>
+                        <input type="text" name="last_name" placeholder="Last name" value="{{old('last_name')}}">
+                        @error('last_name')
+                            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <div class="form-block">
+                        <label for="first_name">Gender</label>
+                        <select name="gender">
+                            <option disabled selected>Select your gender</option>
+                            <option value="male" {{old('gender') === 'male' ? 'selected' : null}}>Male</option>
+                            <option value="female" {{old('gender') === 'female' ? 'selected' : null}}>Female</option>
+                        </select>
+                        @error('gender')
+                            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <div class="form-block">
+                        <label for="last_name">Email</label>
+                        <input type="email" name="email" placeholder="Email" value="{{old('email')}}">
+                        @error('email')
+                            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <div class="form-block">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" placeholder="Password">
+                        @error('password')
+                            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <div class="form-block">
+                        <label for="first_name">Confirm password</label>
+                        <input type="password" name="password_confirmation" placeholder="Confirm password">
+                    </div>
+
+                    <div class="form-block">
+                        <button type="submit" class="mr-1.5">Sign up</button>
+                        <span class="text-sm">
+                            Already have an account?
+                            <a href="/login" class="underline underline-offset-2 ml-1.5">
+                                Log in
+                            </a>
+                        </span>
+                    </div>
+
+                </form>
             </div>
-
-            <div class="form-block">
-                <label for="last_name">Last name</label>
-                <input type="text" name="last_name" placeholder="Last name" value="{{old('last_name')}}">
-                @error('last_name')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
+            <div class="mt-6 px-12 w-1/3">
+                <x-module-socials />
             </div>
-
-            <div class="form-block">
-                <label for="first_name">Gender</label>
-                <select name="gender">
-                    <option disabled selected>Select your gender</option>
-                    <option value="male" {{old('gender') === 'male' ? 'selected' : null}}>Male</option>
-                    <option value="female" {{old('gender') === 'female' ? 'selected' : null}}>Female</option>
-                </select>
-                @error('gender')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
-            </div>
-
-            <div class="form-block">
-                <label for="last_name">Email</label>
-                <input type="email" name="email" placeholder="Email" value="{{old('email')}}">
-                @error('email')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
-            </div>
-
-            <div class="form-block">
-                <label for="password">Password</label>
-                <input type="password" name="password" placeholder="Password">
-                @error('password')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
-            </div>
-
-            <div class="form-block">
-                <label for="first_name">Confirm password</label>
-                <input type="password" name="password_confirmation" placeholder="Confirm password">
-            </div>
-
-            <div class="form-block">
-                <button type="submit">Sign up</button>
-                <span class="text-sm">
-                    Already have an account?
-                    <a href="/login">
-                        Log in
-                    </a>
-                </span>
-            </div>
-
-        </form>
+        </div>
     </x-container>
 </x-layout>
