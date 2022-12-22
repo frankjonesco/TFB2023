fbafe4faed3cf751a10f75b25ab9a600e907f0ad
“Frank <frank@frankjones.co>
2022-12-22 16:53:49 +0100

    Today's Features front-end tidied up a lot

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -91,55 +91,76 @@
         {{-- Featured articles --}}
         <div class="bg-repeat" style="background-image:url('../images/bg.png');">
             <div class="flex w-3/4 mx-auto px-12 bg-white">
-                <div class="mt-3 pl-3 pr-4 w-2/3 border-r border-gray-200">
+                <div class="mt-3 pl-3 pr-4 w-2/3 border-r border-gray-100">
 
                     <div class="flex mb-7">
                         <h3 class="pr-2 pb-3 border-b border-red-500 uppercase text-sm text-gray-800">Today's features</h3>
                         <span class="grow border-b border-gray-200"></span>
                     </div>
 
-                    <div class="flex">
-                        <div class="w-1/2 mr-6">
+                    <div class="flex border-b border-gray-100 mb-3">
+                        <div class="w-1/2 mr-7">
                             @foreach($highlighted_feature_articles as $article)
-                                <div class="bg-no-repeat bg-cover bg-center px-4 py-5 mb-6 flex flex-col justify-end overflow-hidden h-72" style="background-image:linear-gradient(to bottom, rgba(245, 246, 252, 0.0), rgba(0, 0, 0, 0.85)), url('{{asset('images/articles/'.$article->hex.'/'.$article->image)}}');">
-                                    <div class="flex flex-col justify-center items-center h-full">
-                                        <span class="bg-lime-600 w-max px-2 py-1 rounded text-xs font-bold">
-                                            Top Stories
-                                        </span>
-                                        <h3 class="pt-3 pb-3 text-center">
-                                            <a href="/news/articles/{{$article->hex}}/{{$article->slug}}" class="text-zinc-100 hover:!text-zinc-100 hover:!text-opacity-80">
-                                                {{$article->title}}
-                                            </a>
-                                        </h3>
-                                        <span class="text-xs italic">
-                                            <span class="mr-3">
-                                                <i class="fa-regular fa-clock mr-1"></i>
-                                                {{showDate($article->created_at)}}
+                                @if($loop->last)
+                                    @php
+                                        $color = 'bg-lime-600';
+                                    @endphp
+                                    <div class="mb-0">
+                                @else
+                                    @php
+                                        $color = 'bg-pink-600';
+                                    @endphp
+                                    <div class="mb-7">
+                                @endif
+                                
+                                    <div class="bg-no-repeat bg-cover bg-center px-4 py-5 flex flex-col justify-end overflow-hidden h-72" style="background-image:linear-gradient(to bottom, rgba(245, 246, 252, 0.0), rgba(0, 0, 0, 0.85)), url('{{asset('images/articles/'.$article->hex.'/'.$article->image)}}');">
+                                        <div class="flex flex-col justify-center items-center h-full">
+                                            <span class="{{$color}} w-max px-2 py-1 rounded text-xs font-bold">
+                                                Top Stories
                                             </span>
-                                            <span>
-                                                <i class="fa-regular fa-eye mr-1"></i>
-                                                {{$article->views}}
+                                            <h3 class="pt-3 pb-3 text-center">
+                                                <a href="/news/articles/{{$article->hex}}/{{$article->slug}}" class="text-zinc-100 hover:!text-zinc-100 hover:!text-opacity-80">
+                                                    {{$article->title}}
+                                                </a>
+                                            </h3>
+                                            <span class="text-xs italic">
+                                                <span class="mr-3">
+                                                    <i class="fa-regular fa-clock mr-1"></i>
+                                                    {{showDate($article->created_at)}}
+                                                </span>
+                                                <span>
+                                                    <i class="fa-regular fa-eye mr-1"></i>
+                                                    {{$article->views}}
+                                                </span>
                                             </span>
-                                        </span>
+                                        </div>
                                     </div>
                                 </div>
                             @endforeach
                         </div>
                         <div class="w-1/2">
-                            @foreach($featured_articles as $article)
-                                <div class="flex pb-2.5 mb-2.5 w-full border-b border-gray-200">
+                            
+                            @foreach($featured_articles as $key => $article)
+                                @if($loop->last)
+                                    <div class="flex mb-3 w-full border-0">
+                                @else
+                                    <div class="flex pb-3 mb-3 w-full border-b border-gray-100">
+                                @endif
+                                        
                                     <div>
-                                        <div class="w-28 h-20 bg-no-repeat bg-cover bg-center px-4 flex flex-col justify-end overflow-hidden" style="background-image:linear-gradient(to bottom, rgba(245, 246, 252, 0.0), rgba(0, 0, 0, 0.85)), url('{{asset('images/articles/'.$article->hex.'/'.$article->image)}}');"></div>
+                                        <a href="/news/articles/{{$article->hex}}/{{$article->slug}}">
+                                            <div class="w-24 h-20 bg-no-repeat bg-cover bg-center px-4 flex flex-col justify-end overflow-hidden border hover:border-gray-300" style="background-image:linear-gradient(to bottom, rgba(245, 246, 252, 0.0), rgba(0, 0, 0, 0.85)), url('{{asset('images/articles/'.$article->hex.'/'.$article->image)}}');"></div>
+                                        </a>
                                     </div>
                                     <div class="flex flex-col pl-4 self-center">
                                         <span class="text-gray-400 text-xs italic">
                                             Top Stories
                                         </span> 
-                                        <h4 class="pt-1 pb-2 text-gray-500 text-xs font-bold">
+                                        <h4 class="pt-1 pb-1 text-gray-500 text-xs font-bold">
                                             <a href="/news/articles/{{$article->hex}}/{{$article->slug}}" class="text-slate-900 hover:!text-red-500">
                                                 {{$article->title}}
                                             </a>
-                                            </h4>
+                                        </h4>
                                         <span class="text-xs text-gray-400 italic">
                                             <i class="fa-regular fa-clock mr-1"></i>
                                             {{showDate($article->created_at)}}
@@ -149,6 +170,9 @@
                             @endforeach
                             {{-- <button class="w-full mt-2">View all articles</button> --}}
                         </div>
+                    </div>
+                    <div class="w-full text-center">
+                        <button class="btn btn-plain"><i class="fa-solid fa-refresh mr-1.5"></i> View more features</button>
                     </div>
 
                 </div>
--- resources/views/components/module-articles-tabbed-list.blade.php
+++ resources/views/components/module-articles-tabbed-list.blade.php
@@ -3,31 +3,33 @@
         <li class="w-1/3 p-3 bg-red-500 text-white font-bold text-center uppercase" id="tabPopular">
             <a href="#" id="tabbedArticlesPopular" class="text-white" data-toggle="tab">Popular</a>
         </li>
-        <li class="w-1/3 p-3 border-l border-r border-zinc-400 bg-zinc-900 text-center uppercase">
+        <li class="w-1/3 p-3 mx-0.5 bg-zinc-900 font-bold text-center uppercase" style="margin: 0 1px;">
             <a href="#" id="tabbedArticlesRecent" class="text-white" data-toggle="tab" id="tabRecent">Recent</a>
         </li>
-        <li class="w-1/3 p-3 bg-zinc-900 text-center uppercase">
-            <a href="#"  id="tabbedArticlesTop" class="text-white" data-toggle="tab" id="tabTop">Top Reviews</a>
+        <li class="w-1/3 p-3 bg-zinc-900 font-bold text-center uppercase">
+            <a href="#"  id="tabbedArticlesTop" class="!text-white" data-toggle="tab" id="tabTop">Top Reviews</a>
         </li>
     </ul>
-    <div class="tab-content">
+    <div class="tab-content text-gray-600">
 
         <div class="tab-pane active" id="popularArticles">
             <ul class="list-posts">
                 @foreach($articles['popular'] as $article)
                     <li class="p-4 px-6 flex items-center border-b bg-slate-50">
-                        <div class="bg-no-repeat bg-cover bg-center w-32 h-20 mr-6" style="background-image:linear-gradient(to bottom, rgba(245, 246, 252, 0.0), rgba(0, 0, 0, 0.50)), url('{{asset($article->getImageThumbnail())}}');">
-                            
-                        </div>
+                        <a href="/news/articles/{{$article->hex}}/{{$article->slug}}">
+                            <div class="bg-no-repeat bg-cover bg-center w-20 h-16 mr-6 border hover:border-gray-400" style="background-image:linear-gradient(to bottom, rgba(245, 246, 252, 0.0), rgba(0, 0, 0, 0.50)), url('{{asset($article->getImageThumbnail())}}');"></div>
+                        </a>
                         
-                        <div class="post-content w-3/4 overflow-hidden">
+                        <div class="post-content overflow-hidden">
                             <div class="text-sm p-0 mb-2">
-                                <a href="/news/articles/{{$article->hex}}/{{$article->slug}}">
-                                    {{$article->title}}
-                                </a>
+                                <h4 class="text-gray-500 text-xs font-bold">
+                                    <a href="/news/articles/{{$article->hex}}/{{$article->slug}}" class="text-slate-900 hover:!text-red-500">
+                                        {{$article->title}}
+                                    </a>
+                                </h4>
                             </div>
                             <ul class="post-tags">
-                                <li class="text-xs italic"><i class="fa-regular fa-clock mr-1"></i>{{showDate($article->created_at)}}</li>
+                                <li class="text-xs italic text-gray-400"><i class="fa-regular fa-clock mr-1"></i>{{showDate($article->created_at)}}</li>
                             </ul>
                         </div>
                     </li>
@@ -38,22 +40,24 @@
         <div class="tab-pane hidden" id="recentArticles">
             <ul class="list-posts">
                 @foreach($articles['recent'] as $article)
-                    <li class="p-4 px-6 flex items-center border-b bg-gray-900">
-                        <div class="bg-no-repeat bg-cover bg-center w-32 h-20 mr-6" style="background-image:linear-gradient(to bottom, rgba(245, 246, 252, 0.0), rgba(0, 0, 0, 0.50)), url('{{asset($article->getImageThumbnail())}}');">
-                            
-                        </div>
+                <li class="p-4 px-6 flex items-center border-b bg-slate-50">
+                    <a href="/news/articles/{{$article->hex}}/{{$article->slug}}">
+                        <div class="bg-no-repeat bg-cover bg-center w-20 h-16 mr-6 border hover:border-gray-400" style="background-image:linear-gradient(to bottom, rgba(245, 246, 252, 0.0), rgba(0, 0, 0, 0.50)), url('{{asset($article->getImageThumbnail())}}');"></div>
+                    </a>
                         
-                        <div class="post-content w-3/4 overflow-hidden">
-                            <div class="text-sm p-0 mb-2">
-                                <a href="/news/articles/{{$article->hex}}/{{$article->slug}}">
+                    <div class="post-content overflow-hidden">
+                        <div class="text-sm p-0 mb-2">
+                            <h4 class="text-gray-500 text-xs font-bold">
+                                <a href="/news/articles/{{$article->hex}}/{{$article->slug}}" class="text-slate-900 hover:!text-red-500">
                                     {{$article->title}}
                                 </a>
-                            </div>
-                            <ul class="post-tags">
-                                <li class="text-xs italic"><i class="fa-regular fa-clock mr-1"></i>{{showDate($article->created_at)}}</li>
-                            </ul>
+                            </h4>
                         </div>
-                    </li>
+                        <ul class="post-tags">
+                            <li class="text-xs italic text-gray-400"><i class="fa-regular fa-clock mr-1"></i>{{showDate($article->created_at)}}</li>
+                        </ul>
+                    </div>
+                </li>
                 @endforeach                
             </ul>										
         </div>
@@ -61,19 +65,21 @@
         <div class="tab-pane hidden" id="topArticles">
             <ul class="list-posts">
                 @foreach($articles['top'] as $article)
-                    <li class="p-4 px-6 flex items-center border-b bg-gray-900">
-                        <div class="bg-no-repeat bg-cover bg-center w-32 h-20 mr-6" style="background-image:linear-gradient(to bottom, rgba(245, 246, 252, 0.0), rgba(0, 0, 0, 0.50)), url('{{asset($article->getImageThumbnail())}}');">
+                    <li class="p-4 px-6 flex items-center border-b bg-slate-50">
+                        <a href="/news/articles/{{$article->hex}}/{{$article->slug}}">
+                            <div class="bg-no-repeat bg-cover bg-center w-20 h-16 mr-6 border hover:border-gray-400" style="background-image:linear-gradient(to bottom, rgba(245, 246, 252, 0.0), rgba(0, 0, 0, 0.50)), url('{{asset($article->getImageThumbnail())}}');"></div>
+                        </a>
                             
-                        </div>
-                        
-                        <div class="post-content w-3/4 overflow-hidden">
+                        <div class="post-content overflow-hidden">
                             <div class="text-sm p-0 mb-2">
-                                <a href="/news/articles/{{$article->hex}}/{{$article->slug}}">
-                                    {{$article->title}}
-                                </a>
+                                <h4 class="text-gray-500 text-xs font-bold">
+                                    <a href="/news/articles/{{$article->hex}}/{{$article->slug}}" class="text-slate-900 hover:!text-red-500">
+                                        {{$article->title}}
+                                    </a>
+                                </h4>
                             </div>
                             <ul class="post-tags">
-                                <li class="text-xs italic"><i class="fa-regular fa-clock mr-1"></i>{{showDate($article->created_at)}}</li>
+                                <li class="text-xs italic text-gray-400"><i class="fa-regular fa-clock mr-1"></i>{{showDate($article->created_at)}}</li>
                             </ul>
                         </div>
                     </li>
@@ -84,7 +90,7 @@
     </div>
 
     <a href="/news">
-        <button class="mt-4 w-full">
+        <button class="mt-4 w-full btn btn-plain">
             <i class="fa-solid fa-newspaper mr-1"></i>
             View more articles
         </button>
