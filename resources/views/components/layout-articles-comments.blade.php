95137ac921019f6ebe5e73c27007502384f00216
“Frank <frank@frankjones.co>
2022-12-28 19:49:55 +0100

    Comment layout component sorted for now.

--- resources/views/components/layout-articles-comments.blade.php
+++ resources/views/components/layout-articles-comments.blade.php
@@ -3,10 +3,10 @@
     <x-layout-heading heading="Comments" class="heading-mt" />
     <div id="commentsList">
         @foreach($article->publicComments as $comment)
-            <div class="flex text-gray-600 text-sm mb-6">
+            <div class="flex text-gray-600 text-sm mb-6 w-full">
                 <img src="{{$comment->authorImage()}}" alt="" class="w-14 h-14 mr-3 rounded-full border border-gray-400">
                 <div>
-                    <div class="comment-bubble rounded ml-3 mb-2.5 px-4 py-2 italic bg-slate-50 !border-stone-200">
+                    <div class="comment-bubble w-full rounded ml-3 mb-2.5 px-4 py-2 italic bg-slate-50 !border-stone-200">
                         {!!linkify($comment->body)!!}
                     </div>
                     <div class="ml-7 text-xs">
@@ -69,8 +69,8 @@
                 })
                 .then(response => response.json())
                 .then(result => {
-                    // console.log('Success:', result);
-                    document.getElementById('commentsList').innerHTML += '<div class="flex text-gray-600 text-sm mb-6"><img src="" alt="" class="w-14 h-14 mr-3 rounded-full border border-gray-400"><div><div class="comment-bubble rounded w-full ml-3 mb-2.5 px-4 py-2 italic bg-slate-50 !border-stone-200">{!!linkify("' + result.body + '")!!}</div><div class="ml-7 text-xs"><div class="mt-1 text-xs italic text-gray-400"><i class="fa-solid fa-user mr-1.5"></i>' + result.author_name + ' - Just now</div></div></div></div>';
+                    console.log('Success:', result);
+                    document.getElementById('commentsList').innerHTML += '<div class="flex text-gray-600 text-sm mb-6"><img src="{{$comment->authorImage()}}" alt="" class="w-14 h-14 mr-3 rounded-full border border-gray-400"><div><div class="comment-bubble rounded w-full ml-3 mb-2.5 px-4 py-2 italic bg-slate-50 !border-stone-200">{!!linkify("' + result.body + '")!!}</div><div class="ml-7 text-xs"><div class="mt-1 text-xs italic text-gray-400"><i class="fa-solid fa-user mr-1.5"></i>' + result.author_name + ' - Just now</div></div></div></div>';
                     document.getElementById('commentName').value = null;
                     document.getElementById('commentEmail').value = null;
                     document.getElementById('commentBody').value = null;
