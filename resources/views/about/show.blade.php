8dbed5109ec658d169ea2a045856c5e432b9e2b3
“Frank <frank@frankjones.co>
2022-12-28 10:53:14 +0100

    About view styled

--- resources/views/about/show.blade.php
+++ resources/views/about/show.blade.php
@@ -1,15 +1,10 @@
 <x-layout>
     <x-container>
-        <div class="flex">
-            <div class="mt-6 pl-3 pr-12 w-2/3 border-r border-zinc-500">                
-                <div class="flex mb-7">
-                    <h3 class="pl-1.5 pr-4 pb-3 border-b border-sky-500 uppercase text-lg">About</h3>
-                    <span class="grow border-b border-zinc-500"></span>
-                </div>
-            </div>
-            <div class="mt-6 px-12 w-1/3">
+        <x-layout-main-area>
+            <x-layout-heading heading="About" />
+        </x-layout-main-area>
+        <x-layout-sidebar>
                 <x-module-socials />
-            </div>
-        </div>
+        </x-layout-sidebar>
     </x-container>
 </x-layout>
